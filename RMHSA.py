00fd558c3a2dcaded4f7f6b148139d251d286a98
Yulan Ma <133615448+mylbuaa@users.noreply.github.com>
2023-09-16 10:17:24 +0800

    Create RMHSA.py

---
+++ RMHSA.py
@@ -0,0 +1,33 @@
+
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+
+
+
+class RegionAttention(nn.Module):
+    def __init__(self, dim, num_heads=6, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x,x2):
+        x2=torch.unsqueeze(x2,1) 
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        attn =attn+x2
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
